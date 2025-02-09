85705a5cf859f5ca5132d83c73e7cd2580279cb5
Meghana Noojipady <m.noojipady@gmail.com>
2025-02-08 19:32:19 -0500

    Update

--- src/auton.cpp
+++ src/auton.cpp
@@ -6,17 +6,17 @@
 #include "subsystems_cpp_files/lady_brown.hpp"
 
 void autonomous() {
-    //Skills Auton
     /*
+    //Skills Auton
     intake_motor.move(127);
-    pros::delay(130);
+    pros::delay(135);
     intake_motor.move(0);
     pros::delay(500);
     chassis.moveToPoint(0, 14, 1700);
     chassis.waitUntilDone();
     chassis.turnToHeading(-85, 2000);
     chassis.waitUntilDone();
-    chassis.moveToPoint(16, 10, 2000, {.forwards=false, .maxSpeed = 40});
+    chassis.moveToPoint(16, 11.5, 2000, {.forwards=false, .maxSpeed = 40});
     chassis.waitUntilDone();
     tipper_piston.toggle();
     chassis.waitUntilDone();
@@ -67,12 +67,65 @@ void autonomous() {
     chassis.waitUntilDone();
     chassis.moveToPoint(-6, 74, 2500);
     chassis.waitUntilDone();
-*/
-    //Match Auton Blue left
+    */
+
+    //New Blue Left Match Auton (Goal Rush)
     /*
+    chassis.moveToPoint(-0.190, 13.58, 1000);
+    chassis.waitUntilDone();
+    ladyBrownState=ForwardState;
+    pros::delay(900);
+    ladyBrownState=StartingState;
+    pros::delay(500);
+    chassis.waitUntilDone();
+    chassis.moveToPoint(0.566, -21.986, 2000, {.forwards=false, .maxSpeed=50});
+    chassis.waitUntilDone();
+    intake_motor.move(127);
+    tipper_piston.toggle();
+    pros::delay(500);
+    chassis.waitUntilDone();
+    chassis.turnToHeading(126, 1500);
+    chassis.waitUntilDone();
+    chassis.moveToPoint(25.05, -39.3, 8000);
+    chassis.waitUntilDone();
+    intake_motor.move(0);
+    */
+    
+    /*
+    intake_motor.move(127);
+    chassis.turnToHeading(-198.77, 900);
+    chassis.waitUntilDone();
+    chassis.moveToPoint(30, -45.097, 1400);
+    chassis.waitUntilDone();
+    intake_motor.move(0);
+    chassis.moveToPoint(39.11, -63.5, 1400);
+    chassis.waitUntilDone();
+    chassis.turnToHeading(-302.93, 1000);
+    chassis.waitUntilDone();
+    chassis.moveToPoint(30.02, -69.865, 1500, {.forwards=false});
+    chassis.waitUntilDone();
+    tipper_piston.toggle();
+    pros::delay(200);
+    chassis.waitUntilDone();
+    intake_motor.move(127);
+    chassis.moveToPoint(37.34, -64.95, 1000);
+    chassis.waitUntilDone();
+    tipper_piston.toggle();
+    //Match Auton Blue left
+    */
+    /*
+    pros::delay(3000);
+    chassis.waitUntilDone();
+    chassis.moveToPoint(-0.167, 13.58, 1000);
+    chassis.waitUntilDone();
+    ladyBrownState=ForwardState;
+    pros::delay(900);
+    ladyBrownState=StartingState;
+    pros::delay(500);
+    chassis.waitUntilDone();
     chassis.moveToPoint(0, -22.24, 1000, {.forwards=false});
     chassis.waitUntilDone();
-    chassis.turnToHeading(33, 700);
+    chassis.turnToPoint(-7.45, -34, 1000, {.forwards=false});
     chassis.waitUntilDone();
     chassis.moveToPoint(-7.45, -34, 1000, {.forwards=false, .maxSpeed=40});//move to first goal
     chassis.waitUntilDone();
@@ -83,6 +136,8 @@ void autonomous() {
     chassis.waitUntilDone();
     intake_piston.toggle();
     intake_motor.move(127);
+    pros::delay(200);
+    chassis.waitUntilDone();
     chassis.moveToPoint(-34.3, -24.9, 1500, {.maxSpeed=100});//move to colored stake
     chassis.waitUntilDone();
     intake_piston.toggle();
@@ -120,6 +175,7 @@ void autonomous() {
     chassis.moveToPoint(-25.171, -61.16, 1500, {.forwards=false});//touch bar
     chassis.waitUntilDone();
 */
+/*
 //Match Auton Blue Right
 chassis.moveToPoint(-0.08, 14, 1400); //move forward to colored stake
 chassis.waitUntilDone();
@@ -130,7 +186,7 @@ chassis.moveToPoint(3, 0, 1300, {.forwards=false});
 chassis.waitUntilDone();
 chassis.turnToHeading(3.4, 800);
 chassis.waitUntilDone();
-chassis.moveToPoint(0.32, -21.79, 1300, {.forwards=false, .maxSpeed=35});//move to goal and tip it
+chassis.moveToPoint(0.32, -21.79, 1300, {.forwards=false, .maxSpeed=50});//move to goal and tip it
 chassis.waitUntilDone();
 tipper_piston.toggle();
 pros::delay(400);
@@ -146,13 +202,13 @@ pros::delay(1000);
 chassis.waitUntilDone();
 chassis.turnToHeading(-210.4, 1200);//turn to stack of four
 chassis.waitUntilDone();
-chassis.moveToPoint(-18.5, -61.5, 2000);//move to stack
+chassis.moveToPoint(-18.5, -61.8, 2000);//move to stack
 chassis.waitUntilDone();
 pros::delay(1000);
 chassis.waitUntilDone();
 chassis.turnToHeading(-156.3, 1000);//turn to gt right ring
 chassis.waitUntilDone();
-chassis.moveToPoint(-22.6, -63.2, 1500);
+chassis.moveToPoint(-22.6, -63.4, 1500);
 chassis.waitUntilDone();
 pros::delay(800);
 chassis.waitUntilDone();
@@ -161,9 +217,12 @@ chassis.waitUntilDone();
 chassis.turnToHeading(-63.35, 1000);
 chassis.waitUntilDone();
 chassis.moveToPoint(13.64, -51.99, 1500, {.forwards=false});//touch bar
+*/
 
   //Match Auton Red Left Not Official
   /*
+  pros::delay(3000);
+  chassis.waitUntilDone();
   chassis.moveToPoint(0, -26.6, 2000, {.forwards=false});
   chassis.waitUntilDone();
   chassis.moveToPoint(0, -36.6, 1500, {.forwards=false, .maxSpeed=40});
@@ -191,15 +250,14 @@ chassis.moveToPoint(13.64, -51.99, 1500, {.forwards=false});//touch bar
   chassis.moveToPoint(32.3, -54.8, 1000);
   chassis.waitUntilDone();
   chassis.moveToPoint(6.9, -48.42, 1000, {.forwards=false});
-  */
-
+*/
   //Match Auton Red Right Not Official
   /*
   chassis.moveToPoint(0, -19.46, 1000, {.forwards=false});
     chassis.waitUntilDone();
     chassis.turnToHeading(-32.36, 700);
     chassis.waitUntilDone();
-    chassis.moveToPoint(7.65, -36.18, 1200, {.forwards=false, .maxSpeed=40});//move to first goal
+    chassis.moveToPoint(7.45, -36.18, 1200, {.forwards=false, .maxSpeed=40});//move to first goal
     chassis.waitUntilDone();
     tipper_piston.toggle();
     pros::delay(300);
@@ -208,7 +266,7 @@ chassis.moveToPoint(13.64, -51.99, 1500, {.forwards=false});//touch bar
     chassis.waitUntilDone();
     intake_piston.toggle();
     intake_motor.move(127);
-    chassis.moveToPoint(30.82, -22.055, 1500, {.maxSpeed=80});//move to colored stake
+    chassis.moveToPoint(33.82, -22.055, 1500, {.maxSpeed=80});//move to colored stake
     chassis.waitUntilDone();
     intake_piston.toggle();
     chassis.moveToPoint(27.068, -24.85, 1500, {.forwards=false});//move back to intake better    intake_piston.toggle();
--- src/subsystems_cpp_files/lady_brown.cpp
+++ src/subsystems_cpp_files/lady_brown.cpp
@@ -18,7 +18,7 @@ void currentTask(void * param) {
             lady_brown_motor.move(((171*100)-rotation_sensor.get_angle()) * 0.009);
         }
         if (ladyBrownState==SecondState) {
-            lady_brown_motor.move(((207.5*100)-rotation_sensor.get_angle()) * 0.025);
+            lady_brown_motor.move(((212*100)-rotation_sensor.get_angle()) * 0.025);
         }
         if (ladyBrownState==ForwardState) {
             lady_brown_motor.move(((314*100)-rotation_sensor.get_angle()) * 0.02);
